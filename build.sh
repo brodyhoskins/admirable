a225d956ddfd3a272eb4cf183e6dea39ee48ef96
Brody <brodyhoskins@users.noreply.github.com>
2024-10-22 22:05:09 -0700

    Generate combined.list, domains.list

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+combined_file="combined.list"
 domains_file="domains.list"
 ipv4_ips_file="ips.ipv4.list"
 ipv6_ips_file="ips.ipv6.list"
-combined_file="combined.list"
 source_file="sources.list"
 
 while IFS= read -r url; do
@@ -12,8 +12,8 @@ done < "$source_file"
 
 sort -u "$combined_file" -o "$combined_file"
 
-grep -E '^[^0-9].*' "$combined_file" > "$ipv4_ips_file"
+grep -E '([0-9]{1,3}\.){3}[0-9]{1,3}' "$combined_file" > "$ipv4_ips_file"
+grep -E '([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|::([0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}' \
+        "$combined_file" > "$ipv6_ips_file"
 
-grep -E '([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|::([0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}' "$combined_file" > "$ipv6_ips_file"
-
-rm "$combined_file"
+grep -v -f "$ipv4_ips_file" "$combined_file" | grep -v -f "$ipv6_ips_file" > "${domains_file}"
